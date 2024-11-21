4db8be4f3ebdf06873d24cb7c4665d76e541c539
Younes <Oussaifi.younes@gmail.com>
2024-11-21 13:12:39 +0100

    Update Reigen-GUI-fileop.ps1

--- Script/PowerShell GUI for File Operations/Reigen-GUI-fileop.ps1
+++ Script/PowerShell GUI for File Operations/Reigen-GUI-fileop.ps1
@@ -1,16 +1,17 @@
-Add-Type -AssemblyName PresentationFramework
+# Load the necessary assembly for Windows Forms
+Add-Type -AssemblyName 'System.Windows.Forms'
 
 # Create OpenFileDialog
 $FileDialog = New-Object System.Windows.Forms.OpenFileDialog
 $FileDialog.Filter = "All files (*.*)|*.*"
 $FileDialog.Multiselect = $true
 
-if ($FileDialog.ShowDialog() -eq "OK") {
+if ($FileDialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
     $SelectedFiles = $FileDialog.FileNames
 
     # Create FolderBrowserDialog
     $FolderDialog = New-Object System.Windows.Forms.FolderBrowserDialog
-    if ($FolderDialog.ShowDialog() -eq "OK") {
+    if ($FolderDialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
         $DestinationFolder = $FolderDialog.SelectedPath
 
         # Copy selected files to destination
