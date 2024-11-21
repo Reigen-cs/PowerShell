1b9a824a8ea14a6740ff7ce2061c8865cc2d5faf
Younes <Oussaifi.younes@gmail.com>
2024-11-21 13:10:01 +0100

    Update SystemMonitoringDirect.ps1

--- Script/Interactive and directly/SystemMonitoringDirect.ps1
+++ Script/Interactive and directly/SystemMonitoringDirect.ps1
@@ -1,22 +1,31 @@
 # Version 1: Monitoring system information directly
 
 # Get CPU usage
-$cpuUsage = Get-Counter '\Processor(_Total)\% Processor Time'
+$cpuUsage = Get-Counter -Counter '\Processor Information(_Total)\% Processor Time' -ErrorAction SilentlyContinue
+if ($cpuUsage) {
+    $cpuUsageValue = $cpuUsage.CounterSamples[0].CookedValue
+    Write-Host "---- CPU Usage: $([math]::round($cpuUsageValue, 2))%"
+} else {
+    Write-Host "---- CPU Usage: Data not available"
+}
 
 # Get memory usage
 $memoryUsage = Get-WmiObject -Class Win32_OperatingSystem | Select-Object -ExpandProperty FreePhysicalMemory
 $totalMemory = Get-WmiObject -Class Win32_OperatingSystem | Select-Object -ExpandProperty TotalVisibleMemorySize
 $memoryUsed = $totalMemory - $memoryUsage
 
+Write-Host "---- Memory Usage: $([math]::round(($memoryUsed / 1MB), 2)) MB / $([math]::round(($totalMemory / 1MB), 2)) MB"
+
 # Get disk usage
 $diskUsage = Get-WmiObject -Class Win32_LogicalDisk | Where-Object {$_.DriveType -eq 3} | Select-Object DeviceID, @{Name='Used Space (GB)';Expression={[math]::round($_.Size / 1GB, 2)}}, @{Name='Free Space (GB)';Expression={[math]::round($_.FreeSpace / 1GB, 2)}}
 
-# Get network usage
-$networkUsage = Get-Counter '\Network Interface(*)\Bytes Total/sec'
-
-# Output results
-Write-Host "---- CPU Usage: $($cpuUsage.CounterSamples[0].CookedValue)%"
-Write-Host "---- Memory Usage: $([math]::round(($memoryUsed / 1MB), 2)) MB / $([math]::round(($totalMemory / 1MB), 2)) MB"
 Write-Host "---- Disk Usage:"
 $diskUsage | ForEach-Object {Write-Host "$($_.DeviceID): $($_.'Used Space (GB)') GB used, $($_.'Free Space (GB)') GB free"}
-Write-Host "---- Network Usage: $($networkUsage.CounterSamples[0].CookedValue) bytes/sec"
+
+# Get network usage
+$networkUsage = Get-Counter -Counter '\Network Interface(*)\Bytes Total/sec' -ErrorAction SilentlyContinue
+if ($networkUsage) {
+    Write-Host "---- Network Usage: $([math]::round($networkUsage.CounterSamples[0].CookedValue, 2)) bytes/sec"
+} else {
+    Write-Host "---- Network Usage: Data not available"
+}
